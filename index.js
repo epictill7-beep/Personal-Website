8dd3068942c5c401d4fee2aca7b263f55b537883
legen <epictill7@gmail.com>
2026-07-13 20:29:35 -0400

    i got an idea to make the website have a match cursor and the buttons will be ropes to looney tunes bombs

--- index.js
+++ index.js
@@ -1,6 +1,47 @@
 const button = document.getElementById("funButton");
 const sound = document.getElementById("celebrate");
 
+document.addEventListener("mousemove", (e) => {
+    document.documentElement.style.setProperty("--x", `${e.clientX}px`);
+    document.documentElement.style.setProperty("--y", `${e.clientY}px`);
+});
+
 button.addEventListener("click", () => {
     sound.play();
+
+    confetti({
+        particleCount: 200,
+        angle: 60,
+        spread: 70,
+        origin: { x: 0 },
+    });
+    confetti({
+        particleCount: 200,
+        angle: 120,
+        spread: 70,
+        origin: { x: 1 },
+    });
 });
+
+
+/*const duration = 2000;
+    const end = Date.now() + duration;
+
+    (function frame() {
+        confetti({
+            particleCount: 6,
+            angle: 60,
+            spread: 70,
+            origin: { x: 0 },
+        });
+        confetti({
+            particleCount: 6,
+            angle: 120,
+            spread: 70,
+            origin: { x: 1 },
+        });
+    
+        if (Date.now() < end) {
+            requestAnimationFrame(frame);
+        }
+    })();*/
